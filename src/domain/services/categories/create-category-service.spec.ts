4ce1db3f7259ec8c593277b6ee9ce8d54fc7a8c6
Vicentesan <vikom.sanchez@gmail.com>
2025-03-22 15:43:46 -0300

    perf: add tests to create category service

---
+++ src/domain/services/categories/create-category-service.spec.ts
@@ -0,0 +1,62 @@
+import { makeCategory } from '@/test/factories/make-category'
+import { makeServer } from '@/test/factories/make-server'
+import { makeUser } from '@/test/factories/make-user'
+
+import { createCategoryService } from './create-category-service'
+
+let sut: typeof createCategoryService
+
+describe('CreateUserService', () => {
+  beforeAll(async () => {
+    sut = createCategoryService
+  })
+
+  it('should be able to create a valid user', async () => {
+    const user = await makeUser()
+    const server = await makeServer({
+      ownerId: user.id,
+    })
+
+    const result = await sut({
+      name: 'test',
+      serverId: server.id,
+    })
+
+    expect(result).toBeDefined()
+    expect(result.status).toBe('ok')
+    expect(result.code).toBe(201)
+    if (result.status !== 'ok') throw new Error('User not created')
+
+    expect(result.data).toEqual(
+      expect.objectContaining({
+        name: 'test',
+        id: expect.any(String),
+        serverId: server.id,
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      }),
+    )
+  })
+
+  it('should not be able to create a category with an already registered name', async () => {
+    const user = await makeUser()
+    const server = await makeServer({
+      ownerId: user.id,
+    })
+
+    await makeCategory({
+      name: 'test',
+      serverId: server.id,
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const result = await sut({
+      name: 'test',
+      serverId: server.id,
+    })
+
+    expect(result).toBeDefined()
+    expect(result.status).toBe('error')
+    expect(result.code).toBe(409)
+  })
+})
